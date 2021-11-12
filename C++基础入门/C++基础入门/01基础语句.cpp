a5a20796e7f5eeb6e6fc23a0f449b39eb60f489a
如初 <1003019131@qq.com>
2021-11-12 16:31:50 +0800

    new

---
+++ C++基础入门/C++基础入门/01基础语句.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+void func_01() 
+{
+	cout << "Êä³öÄÚÈÝ" << endl;
+}
--- C++基础入门/C++基础入门/main.cpp
+++ C++基础入门/C++基础入门/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
 
+void func_01();
+
 int main()
 {
+	func_01();
 	system("pause");
 	return 0;
 }
