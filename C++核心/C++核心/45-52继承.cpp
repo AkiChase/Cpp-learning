72f38d80aa81a61c88945d64565ce74e285f3f07
如初 <59353056+ruchuby@users.noreply.github.com>
2022-02-03 11:04:02 +0800

    继承结束

--- C++核心/C++核心/45-52继承.cpp
+++ C++核心/C++核心/45-52继承.cpp
@@ -12,7 +12,10 @@ public:
 		cout << "尾部================尾部" << endl;
 	}
 	int n = 666;
+	static int s;
 };
+
+int Base::s = 123;
 
 // content1称为父类Base（基类）的子类（派生类）
 /*
@@ -34,7 +37,10 @@ public:
 		cout << "内容1" << endl;
 	}
 
+	static int s;
 };
+int content1::s = 223;
+
 
 class content2 : public Base
 {
@@ -50,6 +56,51 @@ public:
 	int n = 777;
 };
 
+class Base2
+{
+	void header() {
+		cout << "头部2================头部2" << endl;
+	}
+	void footer() {
+		cout << "尾部2================尾部2" << endl;
+	}
+};
+
+// 多继承
+// 多个父类中可能有同名成员，访问时需要加作用域，否则报错
+// 实际开发中不建议使用
+class content3 : public Base, public Base2
+{
+public:
+	void print() {
+		cout << "内容3" << endl;
+	}
+};
+
+
+// 菱形继承:即一个基类有两个派生类，而这两个派生类又有一个派生自他们俩的派生类
+// 此时两个派生类都有相同的数据，而他们的派生类就会有双份相同的数据，造成浪费，而且访问都要加作用域不方便
+// 解决方法：虚继承，即在两个派生类继承方式上加上  virtual关键字 
+// 这样他们的派生类只有一份数据，写上不同的作用域也会访问到同一份数据，所以作用域可省略
+class Animal
+{
+public:
+	int n = 1;
+};
+
+class Sheep : virtual public Animal
+{
+public:
+	int nn = 2;
+};
+class Tuo : virtual public Animal
+{
+public:
+	int nn = 3;
+};
+
+class YangTuo :public Sheep, public Tuo {};
+
 void func_45() {
 	content1 c1;
 	c1.header();
@@ -64,4 +115,12 @@ void func_45() {
 	cout << c2.n << "\t" << c2.Base::n << endl;
 	c2.print();
 	c2.footer();
+
+	//静态成员通过对象访问通上
+	//静态成员通过类名访问,甚至能通过子类访问，总之就是标明作用域
+	cout << content1::s << "\t" << Base::s << "\t" << content1::Base::s << endl;
+
+	YangTuo yt;
+	cout << yt.n << endl;
+	cout << yt.Sheep::nn << "\t" << yt.Tuo::nn << endl;
 }
